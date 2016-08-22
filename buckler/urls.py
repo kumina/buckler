2e434e93426d6a2546a37038c2d5381934d16d5e
Ed Schouten <ed@kumina.nl>
2016-08-22 09:50:40 +0200

    Disable CSRF protection for the Buckler view.
    We need to pass through HTTP post requests directly without attempting
    to make use of Django's CSRF protection. I suspect that this was
    overlooked during development, as CSRF may have been disabled there.

--- buckler/urls.py
+++ buckler/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import BucklerView, LoginView, LogoutView, InjectJSView
 
@@ -8,5 +9,5 @@ urlpatterns = [
     url(r'^inject\.js$', InjectJSView.as_view(), name='injectjs'),
     url(r'@login$', LoginView.as_view(), name='login'),
     url(r'@logout$', LogoutView.as_view(), name='logout'),
-    url(r'(?P<url>.*)$', BucklerView.as_view())
+    url(r'(?P<url>.*)$', csrf_exempt(BucklerView.as_view()))
 ]
